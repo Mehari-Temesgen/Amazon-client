0807fde8dbe0a63a155d1bb64af4c081c40b9945
Mehari-Temesgen <meharics2011@gmail.com>
2024-02-14 14:48:18 +0300

    back end address add

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: "http://127.0.0.1:5001/clone-3d0d2/us-central1/api",
+  baseURL: "https://amazon-server-vqks.onrender.com",
 });
 
 export default instance;
